6bdeb7d1c8cea86158b3aff6b6fe02b0cccf5137
Amol Nayakawadi <amolnayakawadi1@gmail.com>
2024-01-08 19:41:11 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'Mlproject',
+    version='0.0.1',
+    author='Amol',
+    author_email='amolnayakawadi1@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','pandas']
+)
